ee723880cd6d31be78358639d19349ca38f0f3cf
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-25 22:48:25 +0200

    Registered all models in admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Worker, Position, Task, TaskType
 
-# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "position",
+        "first_name",
+        "last_name",
+        "email"
+    )
+    search_fields = ("username", "email")
+    list_filter = ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_completed", "priority", "deadline",)
+    search_fields = ("name",)
+    list_filter = ("priority", "is_completed", "task_type")
