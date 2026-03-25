e9602505bc7b1ed0e493b772af20076ff7363325
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-25 22:21:27 +0200

    Created models: Position, Worker, TaskType and Task

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+    def __str__(self):
+        return (
+            f"{self.username} ({self.position})" if self.position
+            else f"{self.username} (Not Defined)"
+        )
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "LOW", "Low"
+        MEDIUM = "MEDIUM", "Medium"
+        HIGH = "HIGH", "High"
+        URGENT = "URGENT", "Urgent"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["-deadline"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return (
+            f"{self.name} ({'Done' if self.is_completed else 'In progress'})"
+        )
