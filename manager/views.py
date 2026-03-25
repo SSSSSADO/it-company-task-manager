927eb492d73ef97931a48c6e93f1ca5b520ab5ec
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-25 23:39:30 +0200

    Add statistic on index page

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from manager.models import Position, Worker, TaskType, Task
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "manager/index.html")
+    context = {
+        "num_workers": Worker.objects.count(),
+        "num_tasks": Task.objects.count(),
+        "num_positions": Position.objects.count(),
+        "num_completed_tasks": Task.objects.filter(is_completed=True).count(),
+        "num_open_tasks": Task.objects.filter(is_completed=False).count(),
+    }
+    return render(request, "manager/index.html", context=context)
