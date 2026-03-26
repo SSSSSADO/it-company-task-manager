61bafdc48ebb1261b160cd5d9f709dbbffd3d3b1
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-26 06:36:55 +0200

    Add django debug tollbar

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -17,7 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from it_company_task_manager import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("manager.urls", namespace="manager")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
