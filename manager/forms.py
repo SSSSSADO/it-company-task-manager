a264afa6c7cbdb8edca1729cbd441ba80330cd46
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-26 07:16:15 +0200

    Add create/update forms for all models

--- manager/forms.py
+++ manager/forms.py
@@ -1,2 +1,41 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from manager.models import Position, Worker, TaskType, Task
+
+
+class PositionForm(forms.ModelForm):
+    class Meta:
+        model = Position
+        fields = "__all__"
+
+
+class WorkerCreateForm(UserCreationForm):
+    class Meta:
+        model = Worker
+        fields = ("username", "position", "first_name", "last_name", "email")
+
+
+class WorkerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = ("username", "position", "first_name", "last_name", "email")
+
+
+class TaskTypeForm(forms.ModelForm):
+    class Meta:
+        model = TaskType
+        fields = "__all__"
+
+
+class TaskForm(forms.ModelForm):
+    deadline = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"})
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "description": forms.Textarea()
+        }
