0ee4214b216398745e84ab7072208fbd736a793b
SSSSSADO <grushkovik.danil@gmail.com>
2026-04-02 14:44:13 +0300

    fix STATIC_ROOT

--- it_company_task_manager/settings/base.py
+++ it_company_task_manager/settings/base.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
